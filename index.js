9588e79eb94d58dc07c8bd20c4e265ad2a968054
Nikita623-de <122625934+Nikita623-de@users.noreply.github.com>
2023-06-24 13:46:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 const app = express()
 app.use(cors());
